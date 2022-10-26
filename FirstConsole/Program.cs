e3d097cd6c586682adab7023a2eb922a6c6245ce
vinodh1988 <k.c.vinodh@hotmail.com>
2022-10-26 11:24:18 +0530

    first-commit

---
+++ FirstConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FirstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/**
+ * class Name:
+ * 
+ *   Convention:
+ *             Name the classes starting with uppercase
+ *             
+ *   Rules
+ *             For class names
+ *             Not start with numbers
+ *             No Space
+ *             No special characters except underscore
+ *             
+ *   Method Name:
+ *   
+ *     Follow the same rules and conventions that you follow for
+ *     class
+ *             
+ *   
+ */
