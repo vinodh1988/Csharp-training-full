0920e296e4bd82bc0b063b48f7d8a5c30231742a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-10-26 13:13:07 +0530

    hexadecimal

---
+++ FirstConsole/Literals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsole
+{
+    internal class Literals
+    {
+        static void Main(string[] s) {
+            float f = 34.34f;
+            int x = 0x34;// 3x(16^1)+(4x16^0) =52
+
+            Console.WriteLine(x + " " + f);
+            Console.WriteLine("{0} is the float and {1:X} is the integer", f, x);
+        }
+    }
+}
--- FirstConsole/UserInput.cs
+++ FirstConsole/UserInput.cs
@@ -6,16 +6,16 @@ namespace FirstConsole
 {
     internal class UserInput
     {
-         static void Main(string[] n)
+        static void Main(string[] n)
         {
             Console.WriteLine("Enter Sno:");
-            int sno= Int32.Parse(Console.ReadLine());
+            int sno = Int32.Parse(Console.ReadLine());
             //Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Name:");
-            string name=Console.ReadLine();
-            Console.WriteLine("Sno : "+sno+" Name: "+name);
+            string name = Console.ReadLine();
+            Console.WriteLine("Sno : " + sno + " Name: " + name);
 
-           
+
             Int32 x = sno;//Autoboxing
             int y = x;//unboxing
 
@@ -24,7 +24,9 @@ namespace FirstConsole
             Nullable<Int32> data = null;
 
             float? m = 30.34f;
+            double d = 1.2;
             Single? sample = m;//represents Float
+
         }
     }
 }
