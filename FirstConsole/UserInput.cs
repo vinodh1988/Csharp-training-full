edb6c3cea3fcdfdac3f8590727f9ef545cda0233
vinodh1988 <k.c.vinodh@hotmail.com>
2022-10-26 12:41:44 +0530

    boxing

--- FirstConsole/UserInput.cs
+++ FirstConsole/UserInput.cs
@@ -9,10 +9,15 @@ namespace FirstConsole
          static void Main(string[] n)
         {
             Console.WriteLine("Enter Sno:");
-            int sno=Convert.ToInt32(Console.ReadLine());
+            int sno= Int32.Parse(Console.ReadLine());
+            //Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Name:");
             string name=Console.ReadLine();
             Console.WriteLine("Sno : "+sno+" Name: "+name);
+
+            Int32 x = sno;//Autoboxing
+            int y = x;//unboxing
+           
         }
     }
 }
