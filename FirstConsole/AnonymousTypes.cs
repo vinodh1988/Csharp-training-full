42e515b926b5298d6fe43968aad6b0c8963c7fc7
vinodh1988 <k.c.vinodh@hotmail.com>
2022-10-27 10:55:24 +0530

    anonymous-type

---
+++ FirstConsole/AnonymousTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsole
+{
+    internal class AnonymousTypes
+    {
+        static void Main(string[] n) {
+            var person = new { Sno=1, Name =" Roger", City ="Mumbai"};
+
+            Console.WriteLine(person);
+            Console.WriteLine(person.Name);
+            Console.WriteLine("Type of Person is "+person.GetType());
+
+           // person.Name = "Rakesh";//readonly not assignable
+        }
+    }
+}
