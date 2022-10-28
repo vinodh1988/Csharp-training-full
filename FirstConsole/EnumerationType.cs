f58efd582db7e821240882b80fe7a941db031f9e
vinodh1988 <k.c.vinodh@hotmail.com>
2022-10-28 10:07:39 +0530

    Enum

---
+++ FirstConsole/EnumerationType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace FirstConsole
+{
+    enum Profession { 
+       Teacher=20,
+       Doctor=22,
+       Programmer,
+       Plumber,
+       Accountant=30,
+       Dancer,
+       Singer,
+       Labour
+    }
+    internal class EnumerationType
+    {
+        static void Main(string[] n) {
+            int profession =(int) Profession.Teacher;
+            Console.WriteLine("profession is " + profession);
+            profession = (int)Profession.Dancer;
+            Console.WriteLine("profession is " + profession);
+        }
+    }
+}
